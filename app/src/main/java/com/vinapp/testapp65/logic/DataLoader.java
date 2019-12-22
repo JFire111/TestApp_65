9d34926bd8eff6a35c39abb0ff7b0877e3d22e6a
JFire111 <jfire111@mail.ru>
2019-12-22 20:19:20 +0400

    2019.12.22 Add loading data

--- app/src/main/java/com/vinapp/testapp65/logic/DataLoader.java
+++ app/src/main/java/com/vinapp/testapp65/logic/DataLoader.java
@@ -1,8 +1,65 @@
 package com.vinapp.testapp65.logic;
 
+import com.vinapp.testapp65.logic.data.Specialty;
+import com.vinapp.testapp65.logic.data.Worker;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class DataLoader {
 
-    private final String DATA_URL = "http://gitlab.65apps.com/65gb/static/raw/master/testTask.json";
+    private ArrayList<Specialty> specialties = new ArrayList<>();
+    private ArrayList<Worker> workers = new ArrayList<>();
+    private JSONObject responseJSON;
 
+    public JSONObject requestToServer(String requestUrl) {
+        StringBuffer response = new StringBuffer();
+        String line;
+        try {
+            URL url = new URL(requestUrl);
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            connection.disconnect();
+            reader.close();
+            responseJSON = new JSONObject(response.toString());
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+        return responseJSON;
+    }
 
+    private void setSpecialties(JSONObject jsonObject) {
+        Specialty specialty = new Specialty();
+        JSONArray specialtyJSON;
+        try {
+            specialtyJSON = jsonObject.getJSONArray("specialty");
+            //specialty.setId(specialtyJSON.getJSONObject());
+        } catch (Exception exc){
+            exc.printStackTrace();
+        }
+    }
+
+    private void setWorkers(JSONObject jsonObject) {
+
+    }
+
+    public ArrayList<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public ArrayList<Worker> getWorkers() {
+        return workers;
+    }
 }
--- app/src/main/java/com/vinapp/testapp65/ui/MainActivity.java
+++ app/src/main/java/com/vinapp/testapp65/ui/MainActivity.java
@@ -2,15 +2,19 @@ package com.vinapp.testapp65.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.vinapp.testapp65.R;
+import com.vinapp.testapp65.logic.DataLoader;
 import com.vinapp.testapp65.logic.DatabaseManager;
 import com.vinapp.testapp65.logic.data.Worker;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String DATA_URL = "https://gitlab.65apps.com/65gb/static/raw/master/testTask.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +28,19 @@ public class MainActivity extends AppCompatActivity {
         worker1.setBirthday("11.11.1111");
         worker1.setAge(1111);
 
-        DatabaseManager databaseManager = new DatabaseManager(this);
+        /*DatabaseManager databaseManager = new DatabaseManager(this);
         databaseManager.addWorker(worker1);
         databaseManager.getAllWorkers();
+*/
+        AsyncTask asyncTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                DataLoader dataLoader = new DataLoader();
+                dataLoader.requestToServer(DATA_URL);
+                return null;
+            }
+        };
+
+        asyncTask.execute();
     }
 }
